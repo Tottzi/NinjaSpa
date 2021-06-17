019a7d0922e8ea3c6c0207d1e4d3eb86842ab645
Tottzi <tottzi@gmail.com>
2021-06-17 19:35:47 +0200

    eslint

--- src/test.spec.js
+++ src/test.spec.js
@@ -1,14 +1,14 @@
 import {expect, it } from '@jest/globals'
 import {testCaseFromFile} from './test'
 
-describe('Test', () => {
-  it('Should return ok', (done) =>{
-		expect(testCaseFromFile()).toBe('Ok')
-		done()
-  })
-  it('Should reject', done => {
-		expect(testCase()).not.toBe('ok')
-		done()
-  })
+
+it('Should return ok', (done) =>{
+	expect(testCaseFromFile()).toBe('Ok')
+	done()
 })
+it('Should reject', done => {
+	expect(testCaseFromFile()).not.toBe('ok')
+	done()
+})
+
 
